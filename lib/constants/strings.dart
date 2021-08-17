90252a9a095b8e0fd17871d46cd3f3d1e671fb1d
Mohit Jain <70198503+codedog001@users.noreply.github.com>
2021-08-17 18:08:53 +0530

    Update Server Files - Migrate to DigitalOcean

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -1,4 +1,4 @@
-const String BASE_URL = 'http://198.13.42.185/';
+const String BASE_URL = 'http://143.244.129.37/';
 const String UPLOAD_URL = BASE_URL + 'withgpsupload.php';
 const String DOWNLOAD_URL = BASE_URL + 'withgpsdownload.php';
 const String IMAGE_URL = BASE_URL + 'img';
